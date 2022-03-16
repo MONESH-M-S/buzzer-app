f61e985295408abad7128d16621444b07059fa1e
MoneshMS <moneshsanju@gmail.com>
2022-03-17 05:21:10 +0530

    Added components for admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,17 @@ import { HomeComponent } from './home/home.component';
 import { PrimengModule } from './primeng.module';
 import { MaterialModule } from './material.module';
 import { FormsModule } from '@angular/forms';
+import { AdminComponent } from './admin/admin.component';
+import { AddQuestionComponent } from './admin/add-question/add-question.component';
+import { ViewQuestionComponent } from './admin/view-question/view-question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AdminComponent,
+    AddQuestionComponent,
+    ViewQuestionComponent,
   ],
   imports: [
     BrowserModule,
